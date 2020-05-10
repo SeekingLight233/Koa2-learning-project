ef1539f0e305eec39822716062478ddee9fdd191
Jason <690163223@qq.com>
2020-05-10 20:49:12 +0800

    fix: 判断登录状态下的页面状态

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -4,12 +4,28 @@
  */
 
 const router = require("koa-router")()
-
+/**
+ * 获取用户信息
+ * @param {Object} ctx koa2 ctx
+ */
+function getLoginInfo(ctx) {
+  let data = {
+    isLogin: false,
+  }
+  const userInfo = ctx.session.userInfo
+  if (userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName,
+    }
+  }
+  return data
+}
 router.get("/login", async (ctx, next) => {
-  await ctx.render("login", {})
+  await ctx.render("login", getLoginInfo(ctx))
 })
 router.get("/register", async (ctx, next) => {
-  await ctx.render("register", {})
+  await ctx.render("register", getLoginInfo(ctx))
 })
 
 module.exports = router
